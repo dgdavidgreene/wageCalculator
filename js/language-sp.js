09f11659627d5083064dcd17833f37c4fc6e492d
dgdavidgreene <dgdavidgreene@yahoo.com>
2014-02-04 16:21:14 -0800

    Multi-language files
    Files for English Spanish and their variables.

---
+++ js/language-sp.js
@@ -0,0 +1,62 @@
+
+
+var categoryCode1Desc = "Salario Neto";
+
+var categoryCode2Desc = "Arrendamiento y Renta";
+var subCat2aDesc = "Camión";
+var subCat2bDesc = "Depósito";
+var subCat2cDesc = "Estacionamiento";
+var subCat2dDesc = "Radio";
+var subCat2eDesc = "Otro";
+
+var categoryCode3Desc = "Mantenimiento";
+var subCat3aDesc = "Regular";
+var subCat3bDesc = "Reparar";
+var subCat3cDesc = "Lavado de Camiones";
+var subCat3dDesc = "Otro";
+
+var categoryCode4Desc = "Combustible";
+var subCat4aDesc = "Diesel";
+var subCat4bDesc = "Otro";
+
+var categoryCode5Desc = "Aseguranza";
+var subCat5aDesc = "Camión";
+var subCat5bDesc = "Responsabilidad";
+var subCat5cDesc = "Cargo";
+var subCat5dDesc = "Occ";
+var subCat5eDesc = "PD";
+var subCat5fDesc = "Otro";
+
+var categoryCode6Desc = "Recargos";
+var subCat6aDesc = "DMV";
+var subCat6bDesc = "Registro";
+var subCat6cDesc = "Licencia";
+var subCat6dDesc = "Permiso";
+var subCat6eDesc = "Impuesto";
+var subCat6fDesc = "DTR";
+var subCat6gDesc = "Multa";
+var subCat6hDesc = "Otro";
+
+var weekDesc = "Semana";
+var monthDesc = "Mes";
+var yearDesc = "Año";
+
+var categoryDesc = "Categoría";
+var countDesc = "Cuenta";
+var hourDesc = "Hora";
+
+var hoursWorkedDesc = "Horas Trabajadas";
+var paidDesc = "Pagadas";
+var summaryDesc = "Resumen";
+var perHourDesc = "Por Hora";
+var grossDesc = "Bruto";
+var netDesc = "Neto";
+var expensesDesc = "Gastos";
+
+var itemDesc = "Articulo";
+var costDesc = "Costo";
+var periodDesc = "Periodo";
+var weeklyPayDesc = "Paga Semanal";
+var removeDesc = "Eliminar";
+var addExpenseDesc = "Agregue un Gasto";
+var weeklyExpensesDesc = "Gastos Semanales:";
